d33a900c7913d9f1a212319a761ce01bf0a3f094
furqan <furqanlateef100@gmail.com>
2023-09-28 00:18:13 +0530

    props completed

--- 03Props/src/App.jsx
+++ 03Props/src/App.jsx
@@ -6,11 +6,16 @@ import Card from './components/Card'
 
 function App() {
   const [count, setCount] = useState(0)
+  let userData ={
+    username:"furqanlateef",
+    email:"furqanlateef100@gmail.com"
+  }
 
   return (
     <>
       <h1 className='bg-green-400 text-black p-4 rounded-2xl mb-4'>Tailwind  Test</h1>
-<Card/>
+<Card name = "Furqan" btnText = "clickMe" />
+<Card name =" Ikhlaq " btnText="btn clicked"/>
     </>
   )
 }
